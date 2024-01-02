80fb8496e2720cc927ce93054ac06fb049efa621
Yuri Pomazkin <1yunker@gmail.com>
2024-01-02 21:54:36 +0500

    Added tests and config fixes

--- src/api/v1/base.py
+++ src/api/v1/base.py
@@ -1,4 +1,3 @@
-# import aiofiles
 import logging
 import os
 from datetime import datetime
--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,3 +1,6 @@
+import pytest
+import pytest_asyncio
+
 from fastapi.testclient import TestClient
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
@@ -7,20 +10,21 @@ from db.db import get_session
 from main import app
 from models import Base
 
+TEST_DATABASE_DSN = 'sqlite+aiosqlite:///./src/tests/test.db'
+
 client = TestClient(app=app)
 
 
-async def override_get_session() -> AsyncSession:
-    engine = create_async_engine(
-        str(app_settings.database_dsn),
+def get_engine():
+    return create_async_engine(
+        TEST_DATABASE_DSN,
         echo=app_settings.echo | True,
         future=True
     )
 
-    # async with engine.begin() as conn:
-    #     await conn.run_sync(Base.metadata.drop_all)
-    #     await conn.run_sync(Base.metadata.create_all)
 
+async def override_get_session() -> AsyncSession:
+    engine = get_engine()
     async_session = sessionmaker(
         engine,
         class_=AsyncSession,
@@ -32,6 +36,15 @@ async def override_get_session() -> AsyncSession:
 app.dependency_overrides[get_session] = override_get_session
 
 
+@pytest_asyncio.fixture(scope='module')
+async def prepare_db():
+    engine = get_engine()
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+@pytest.mark.usefixtures('prepare_db')
 def test_register():
     response = client.post(
         app.url_path_for('register'),
@@ -41,21 +54,15 @@ def test_register():
     assert response.json() == {'id': 1, 'email': 'user@example.com'}
 
 
-def test_login_and_get_files_info():
+def test_login(cache):
     response = client.post(
         app.url_path_for('login'),
         data={'username': 'user@example.com', 'password': 'string'},
         headers={'Content-Type': 'application/x-www-form-urlencoded'}
     )
-    assert response.status_code == 200
-    token = response.json().get('access_token')
+    cache.set('access_token', response.json().get('access_token'))
 
-    response = client.get(
-        app.url_path_for('get_files_info'),
-        headers={'Authorization': f'Bearer {token}'}
-    )
     assert response.status_code == 200
-    assert response.json() == {'account_id': 1, 'files': []}
 
 
 def test_get_ping():
@@ -63,3 +70,15 @@ def test_get_ping():
         app.url_path_for('get_ping')
     )
     assert response.status_code == 200
+
+
+@pytest.mark.dependency(depends=['test_login'])
+def test_get_files_info(cache):
+    token = cache.get('access_token', None)
+    response = client.get(
+        app.url_path_for('get_files_info'),
+        headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {'account_id': 1, 'files': []}
